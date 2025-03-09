564777ad42ec28dc8a62028bb96322baaafd8b9f
tcl <cy.tcl@qq.com>
2025-03-09 16:05:24 +0800

    feat：负载均衡

--- tcl-rpc-core/src/main/java/com/tcl/tclrpc/config/RpcConfig.java
+++ tcl-rpc-core/src/main/java/com/tcl/tclrpc/config/RpcConfig.java
@@ -1,5 +1,6 @@
 package com.tcl.tclrpc.config;
 
+import com.tcl.tclrpc.loadbalancer.LoadBalancerKeys;
 import com.tcl.tclrpc.serializer.SerializerKeys;
 import lombok.Data;
 
@@ -40,5 +41,10 @@ public class RpcConfig {
      * 注册中心配置
      */
     private RegistryConfig registryConfig = new RegistryConfig();
+
+    /**
+     * 负载均衡器
+     */
+    private String loadBalancer = LoadBalancerKeys.ROUND_ROBIN;
 
 }
--- tcl-rpc-core/src/main/java/com/tcl/tclrpc/proxy/ServiceProxy.java
+++ tcl-rpc-core/src/main/java/com/tcl/tclrpc/proxy/ServiceProxy.java
@@ -1,32 +1,25 @@
 package com.tcl.tclrpc.proxy;
 
 import cn.hutool.core.collection.CollUtil;
-import cn.hutool.core.util.IdUtil;
-import cn.hutool.http.HttpRequest;
-import cn.hutool.http.HttpResponse;
 import com.tcl.tclrpc.RpcApplication;
 import com.tcl.tclrpc.config.RpcConfig;
 import com.tcl.tclrpc.constant.RpcConstant;
+import com.tcl.tclrpc.loadbalancer.LoadBalancer;
+import com.tcl.tclrpc.loadbalancer.LoadBalancerFactory;
 import com.tcl.tclrpc.model.RpcRequest;
 import com.tcl.tclrpc.model.RpcResponse;
 import com.tcl.tclrpc.model.ServiceMetaInfo;
-import com.tcl.tclrpc.protocol.*;
 import com.tcl.tclrpc.registry.Registry;
 import com.tcl.tclrpc.registry.RegistryFactory;
 import com.tcl.tclrpc.serializer.Serializer;
 import com.tcl.tclrpc.serializer.SerializerFactory;
 import com.tcl.tclrpc.server.tcp.VertxTcpClient;
-import io.netty.util.concurrent.CompleteFuture;
-import io.vertx.core.Vertx;
-import io.vertx.core.buffer.Buffer;
-import io.vertx.core.net.NetClient;
-import lombok.val;
 
-import java.io.IOException;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
+import java.util.Map;
 
 /**
  * 服务代理（JDK 动态代理）
@@ -63,7 +56,14 @@ public class ServiceProxy implements InvocationHandler {
             if (CollUtil.isEmpty(serviceMetaInfoList)) {
                 throw new RuntimeException("暂无服务地址");
             }
-            ServiceMetaInfo selectedServiceMetaInfo = serviceMetaInfoList.get(0);
+
+            // 负载均衡
+            LoadBalancer loadBalancer = LoadBalancerFactory.getInstance(rpcConfig.getLoadBalancer());
+            // 将调用方法名（请求路径）作为负载均衡参数
+            Map<String, Object> requestParams = new HashMap<>();
+            requestParams.put("methodName", rpcRequest.getMethodName());
+            ServiceMetaInfo selectedServiceMetaInfo = loadBalancer.select(requestParams, serviceMetaInfoList);
+
             // 发送 TCP 请求
             RpcResponse rpcResponse = VertxTcpClient.doRequest(rpcRequest, selectedServiceMetaInfo);
             return rpcResponse.getData();
